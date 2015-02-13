0af8c0880d492c81a085c4c547a1443615125f9f
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-02-12 19:19:52 -0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,184 @@
+var gruntConfig = {
+
+	clean: {
+
+		dev: [ 'public' ],
+		deploy: [ 'deploy' ]
+
+	},
+
+	sass: {
+
+		dev: {
+
+			files:
+			[{
+				expand: true,
+				cwd: 'assets/scss',
+				src: ['*.scss'],
+				dest: 'public/styles/src',
+				ext: '.css'
+			}]
+
+		}
+
+	},
+
+	cssmin: {
+
+		dev: {
+			expand: true,
+			cwd: 'public/styles/src',
+			src: ['**/*.css'],
+			dest: 'public/styles/min',
+			ext: '.css'
+		}
+
+	},
+
+	express: {
+
+		dev: {
+
+			options: {
+				script: 'server.js'
+			}
+
+		}
+
+	},
+
+	copy: {
+
+		images: {
+			expand: true,
+			cwd: 'assets/images',
+			src: '**',
+			dest: 'public/images'
+		},
+
+		js: {
+			expand: true,
+			cwd: 'assets/scripts',
+			src: '**',
+			dest: 'public/scripts/src'
+		}
+
+	},
+
+	uglify: {
+    js: {
+      files: [{
+        expand: true,
+        cwd: 'public/scripts/src',
+        src: '*.js',
+        dest: 'public/scripts/min'
+      }]
+    }
+  },
+
+	jade: {
+
+		dev: {
+
+			options: {
+				pretty: true,
+				data: {
+					debug: false,
+					linkPath: ''
+				}
+			},
+
+			expand: true,
+			cwd: 'views',
+			src: ['**/*.jade', '!**/common/**'],
+			dest: 'public/',
+			ext: '.html'
+		},
+
+		deploy: {
+
+			options: {
+				pretty: true,
+				data: {
+					debug: false,
+					linkPath: '<%= pkg.repository.pages %>/public'
+				}
+			},
+
+			expand: true,
+			cwd: 'views',
+			src: ['**/*.jade', '!**/common/**'],
+			dest: 'public/',
+			ext: '.html'
+		}
+
+	},
+
+	watch: {
+
+		express: {
+			files:  [ 'views/**/**/*.jade' ],
+			tasks: ['jade:dev'],
+			options: {
+				livereload: true
+			}
+		},
+
+		sass: {
+			files: ['assets/scss/**/*.scss'],
+			tasks: ['sass:dev', 'cssmin:dev']
+		},
+
+		styles: {
+			files: ['assets/styles/**/*.css'],
+			tasks: ['cssmin:dev']
+		},
+
+		scripts: {
+			files: ['assets/scripts/*.js'],
+			tasks: ['copy:js', 'uglify:js']
+		},
+
+		livereload: {
+			options: {
+				livereload: true
+			},
+			files: [
+				'public/styles/**/*.css',
+				'public/scripts/*js'
+			]
+		}
+
+	}
+
+};
+
+module.exports = function(grunt) {
+
+	gruntConfig.pkg = grunt.file.readJSON('package.json');
+
+	grunt.initConfig(gruntConfig);
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-express-server');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('predeploy', [ 'clean',
+																		'sass',
+																		'copy',
+																		'uglify',
+																		'cssmin'
+																		]);
+
+	grunt.registerTask('default', [ 'predeploy',
+																	'express' ]);
+
+	grunt.registerTask('deploy', [ 'predeploy', 'jade:deploy']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sonos-suit-prototyping-kit
-Rapidly prototype HTML pages with Sonos Suit using Jade, Sass, Grunt and Express
+Sonos Suit Prototyping Kit
+==============
+
+This is the Sonos Suit prototyping kit. To install, clone the repo or download the zip file to your computer. Run `npm install` and the project will install all of the appropriate dependencies. If you want to host your pages on Github, be sure to update the `package.json` file with your Github pages url or leave it blank if you're running your own FTP server.
+
+After installing your dependencies, you can run the `grunt` command to start the local server.
+
+To enable LiveReload, in a separate terminal window, run `grunt watch`.
+
+To generate HTML for a static upload to a server or GitHub pages, run `grunt deploy`.
